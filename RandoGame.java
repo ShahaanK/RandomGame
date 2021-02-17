77e3fc96b135794bcd273afbbd0ba1a1e27a08d2
Shahaan Khan <56699337+ShahaanK@users.noreply.github.com>
2021-02-17 01:21:16 -0500

    Update RandoGame.java
    Created the skeleton of the program.
    Things to add:
    Make it recursively run.
    Make functions work.
    Add catch statements.
    Set up a life system so you only have 5 guesses or something.
    Look up graphics for fun.

--- RandoGame.java
+++ RandoGame.java
@@ -1,35 +1,38 @@
 package timeToGame;
 import java.util.Scanner;
+import java.util.Random;
 
 public class RandoGame {
-	int num;
-	int temp;
-	int high;
-	int low;
-	int userNum = -1;
+	static int randNum;
+	static int userNum = -1;
+	static int flag = 0;
 
-	private void giveNum() {
+	private static void giveNum() {
 		//creates a scanner object
 		Scanner q = new Scanner(System.in);
 		//check if its the first time running it
-		if (this.userNum == -1) {
+		if (userNum == -1) {
 			//assigns the value entered to userNum
-			this.userNum = q.nextInt();
+			userNum = q.nextInt();
+		}
+		else if (flag == 0) {
+			q.close();
 		}
 		//if it isn't the first time running the program, then go here
 		else {
 			System.out.println("Please enter your next guess.");
-			this.userNum = q.nextInt();
+			userNum = q.nextInt();
 		}
-		q.close();
 	}
-	private void checkPos(int x) {
+	
+	private static void checkPos() {
 
-		if (x > this.userNum) {
-
+		if (randNum > userNum) {
+			System.out.println("Too High, Guess again");
 		}
-		else if (x == this.userNum) {
-
+		else if (randNum == userNum) {
+			System.out.println("You Win! The correct answer was: " + userNum);
+			flag = 0;
 		}
 		else {
 			System.out.println("Too Low");
@@ -39,14 +42,35 @@ public class RandoGame {
 
 
 
-
-	public void main(String[] args) {
-		System.out.println("Welcome to the random Game 9000, please enter any whole number.");
-		giveNum();
-
+	public static void main(String[] args) {
+		System.out.println("Welcome to the random Game 9000, please enter any positive number between 0-100.");
+		
+		//create the random number
+		Random rand = new Random();
+		randNum = rand.nextInt(101);
+		//System.out.println(randNum); FIXME this is to reveal number.
+		
+		//input
+		Scanner q = new Scanner(System.in);
+		
+		while (flag == 0) {
+			userNum = q.nextInt();
+			
+			if (randNum < userNum) {
+				System.out.println("Too High, Guess again");
+			}
+			else if (randNum == userNum) {
+				System.out.println("You Win! The correct answer was: " + userNum);
+				flag = 1;
+			}
+			else {
+				System.out.println("Too Low");
+			}
+			
+			
+		}
+		
+		q.close();
 		//check if its higher or lower recursively
 	}
-
-
-
 }
