2d6a0e35340b6d77fd053cea4b5ebcf8f45e44e4
Shahaan Khan <56699337+ShahaanK@users.noreply.github.com>
2021-02-17 21:04:47 -0500

    Update RandoGame.java
    Removed all functions and created a working game with a chance system. Had a bug where it wouldn't give you the winner msg if you were on your last guess. Fixed it. Next steps: Graphics, popup msg on win, maybe levels (easy, med, hard), try to work on functions.

--- RandoGame.java
+++ RandoGame.java
@@ -5,72 +5,57 @@ import java.util.Random;
 public class RandoGame {
 	static int randNum;
 	static int userNum = -1;
-	static int flag = 0;
-
-	private static void giveNum() {
-		//creates a scanner object
-		Scanner q = new Scanner(System.in);
-		//check if its the first time running it
-		if (userNum == -1) {
-			//assigns the value entered to userNum
-			userNum = q.nextInt();
-		}
-		else if (flag == 0) {
-			q.close();
-		}
-		//if it isn't the first time running the program, then go here
-		else {
-			System.out.println("Please enter your next guess.");
-			userNum = q.nextInt();
-		}
-	}
-	
-	private static void checkPos() {
-
-		if (randNum > userNum) {
-			System.out.println("Too High, Guess again");
-		}
-		else if (randNum == userNum) {
-			System.out.println("You Win! The correct answer was: " + userNum);
-			flag = 0;
-		}
-		else {
-			System.out.println("Too Low");
-		}
-
-	}
-
-
+	static boolean flag = true;
+	static int chances = 7;
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to the random Game 9000, please enter any positive number between 0-100.");
-		
+		//The game output
+		System.out.println("Welcome to the Random Game 9000.\n");
+
 		//create the random number
 		Random rand = new Random();
+		//assign range from 0-100 for random value
 		randNum = rand.nextInt(101);
-		//System.out.println(randNum); FIXME this is to reveal number.
-		
-		//input
-		Scanner q = new Scanner(System.in);
-		
-		while (flag == 0) {
-			userNum = q.nextInt();
-			
-			if (randNum < userNum) {
-				System.out.println("Too High, Guess again");
+
+		System.out.println(randNum); //FIXME this is to reveal number.
+
+		//set input value
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please enter any positive number between 0-100. You have 7 chances to guess the correct answer.");
+
+		//check input
+		while (flag) {
+			try {
+				userNum = input.nextInt();		
+			} 
+			catch (Exception e) {
+				System.out.println("Please enter a valid number. You have " + chances + " chances left.");	
 			}
-			else if (randNum == userNum) {
-				System.out.println("You Win! The correct answer was: " + userNum);
-				flag = 1;
+
+
+			if (chances >= 0) {
+				if (randNum < userNum) {
+					System.out.println("Too High. Guess again!. You have " + chances + " chances left.");
+					chances--;
+				}
+				
+				else if (randNum == userNum) {
+					System.out.println("You Win! The correct answer was: " + userNum);
+					flag = false;
+				}
+				
+				else if (randNum > userNum){
+					System.out.println("Too Low. Guess again! You have " + chances + " chances left.");
+					chances--;
+				}
 			}
+
 			else {
-				System.out.println("Too Low");
+				System.out.println("You have run out of lives. Thank you for playing. " + randNum + " was the random number.");
+				flag = false;
 			}
-			
-			
 		}
-		
-		q.close();
-		//check if its higher or lower recursively
+		input.close();
 	}
 }
+
